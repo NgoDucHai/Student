4e707215947950c3ae631e7cb41a8c71b81dc6ab
Ngo Anh Long <ngoanhlong@gmail.com>
2015-07-23 08:50:57 +0700

    Edit migration file

--- migrations/20150722050509_student_profile.php
+++ migrations/20150722050509_student_profile.php
@@ -5,15 +5,23 @@ use Phinx\Migration\AbstractMigration;
 class StudentProfile extends AbstractMigration {
 
     public function change() {
-        $table = $this->table('student');
-        $table->addColumn('studentId', 'string', ['limit' => 25])
+
+        $exists = $this->hasTable('student');
+        if ($exists) {
+            $this->dropTable('student');
+        }
+
+        $table = $this->table('student', ['id' => false]);
+        $table
                 ->addColumn('studentName', 'string', ['limit' => 50])
                 ->addColumn('dateOfBirth', 'date')
                 ->addColumn('gender', 'boolean')
                 ->addColumn('phone', 'integer', ['limit' => 11])
                 ->addColumn('address', 'string', ['limit' => 150])
-                ->addIndex(array('studentId'), array('unique' => true))
                 ->create();
+
+        //addColumn('studentId', 'string', ['limit' => 25])
+        $this->execute("ALTER TABLE student ADD COLUMN studentId varchar(25) PRIMARY KEY;");
     }
 
 }
