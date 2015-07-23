e8735b88f7f80a42dfe0b9ad6b04dc2b7dd0b96e
Ngo Anh Long <ngoanhlong@gmail.com>
2015-07-23 11:47:41 +0700

    Long's Commit

--- application/modules/student/controllers/ProfileController.php
+++ application/modules/student/controllers/ProfileController.php
@@ -1,23 +1,27 @@
 <?php
 
-class Student_ProfileController extends Zend_Controller_Action
-{
+class Student_ProfileController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
     }
-    
-    public function createAction()
-    {
+
+    public function createProfileAction() {
         $this->view->headTitle("create profile");
-        $form = new Student_Form_Create();
+        $form = new Student_Form_CreateProfile();
+        /* @var $request Zend_Controller_Request_Http*/
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            if($form->isValid($request->getPost()))
+            {
+                echo "Ok you done";
+            }
+        }
         $this->view->form = $form;
     }
-}
 
+}
--- tests/application/CreateProfileTest.php
+++ tests/application/CreateProfileTest.php
@@ -10,7 +10,6 @@ class CreateProfileTesting extends Vms_Test_PHPUnit_ControllerWithDatabaseFixtur
 
     // Male is true OR 1 
     // Female is false OR 0
-    
     // Date of Birth format: yyyy/mm/dd
 
 
@@ -49,77 +48,81 @@ class CreateProfileTesting extends Vms_Test_PHPUnit_ControllerWithDatabaseFixtur
     }
 
     public function testWhenUserAccessThenExpectedHttpCodeIs200() {
-        $this->dispatch('student/profile/create');
+        $this->dispatch('student/profile/create-profile');
 
         $this->assertResponseCode(200);
-        $this->assertAction('create');
+        $this->assertAction('create-profile');
         $this->assertModule('student');
         $this->assertController('profile');
     }
 
     public function testWhenUserAccessThenExpectedPageTitleFollowCustomerIdea() {
         $customerIdea = "create profile";
-        $this->dispatch('student/profile/create');
+        $this->dispatch('student/profile/create-profile');
 
         $this->assertQueryContentContains('title', $customerIdea);
     }
 
     public function testWhenUserAccessThenExpectedPageHasAFormAndFieldsNeedToFill() {
-        $this->dispatch('student/profile/create');
+        $this->dispatch('student/profile/create-profile');
 
         $this->assertXpath('//form[@name="createProfile"]');
         $this->assertXpath('//input[@type="text"][@name="studentId"]');
-        $this->assertXpath('//input[@type="date"][@name="dateOfBirth"]');
-        $this->assertXpathCount('//input[@type="radio"][@name="gender"]', 2);
+        $this->assertXpath('//input[@type="text"][@name="dateOfBirth"]');
+        $this->assertXpath('//select[@name="gender"]');
         $this->assertXpath('//input[@type="text"][@name="phone"]');
-        $this->assertXpath('//textarea[@row="4"][@cols="5"]');
+        $this->assertXpath('//textarea[@rows="10"][@cols="40"]');
         $this->assertXpath('//input[@type="submit"]');
     }
 
     public function testIfUserCreateUserHasAStudentIDExistsThenExpectedPageContentContainsMessageError() {
-        $this->getRequest()->setPost(
-            ['studentId' => '12020535'
+        $this->request->setMethod("POST")
+                ->setPost(
+                ['studentId' => '12020535',
+                    'studentName' => 'Ngo Anh Long',
+                    'dateOfBirth' => '1994/04/18',
+                    'gender' => 1,
+                    'phone' => '0166 366 9281',
+                    'address' => 'Quang Ninh'
         ]);
-        
-        $this->dispatch('/student/profile/create');
+
+        $this->dispatch('/student/profile/create-profile');
         $this->assertQueryContentContains('body', 'Your studentId is Exists');
     }
-    
-    public function testIfUserCreateAUserNotExistsInDBThenExpectedPageContentContainsMessageSuccess()
-    {
-        $this->getRequest()->setPost(
-            ['studentId' => '12353473',
+
+    public function testIfUserCreateAUserNotExistsInDBThenExpectedPageContentContainsMessageSuccess() {
+        $this->request->setMethod("POST")
+                ->setPost(
+                ['studentId' => '12353473',
                     'studentName' => 'Ngo Anh',
                     'dateOfBirth' => '1995/04/18',
                     'gender' => 1,
                     'phone' => '0166 366 9999',
                     'address' => 'Quang Ninh'
-                ]);
-        
+        ]);
+
         $this->dispatch('/student/profile/create');
         $this->assertQueryContentContains('body', "Ok you've created a User");
     }
-    
-    public function testIfUserInputEmptyValueIntoRequiredFieldThenDisplayAErrorMessage()
-    {
-        $this->getRequest()->setPost(
-            ['studentId' => '',
-                    'studentName' => '',
-                    'dateOfBirth' => '',
-                    'gender' => '',
-                    'phone' => '',
-                    'address' => ''
-                ]);
-        
-        $this->dispatch('/student/profile/create');
-        $this->assertQueryContentContains('li', "studentName is required");
-        $this->assertQueryContentContains('li', "dateOfBirth is required");
-        $this->assertQueryContentContains('li', "gender is required");
-        $this->assertQueryContentContains('li', "phone is required");
-        $this->assertQueryContentContains('li', "address is required");
-        $this->assertQueryContentContains('li', "studentId is required");
-        
+
+    public function testIfUserInputEmptyValueIntoRequiredFieldThenDisplayAErrorMessage() {
+        $this->request->setMethod('POST')
+              ->setPost([
+                  'studentId' => '',
+                  'studentName' => '',
+                  'dateOfBirth' => '',
+                  'gender' =>'',
+                  'phone' =>'',
+                  'address' => ''
+              ]);
+
+        $this->dispatch('/student/profile/create-profile');
+        $this->assertQueryContentContains('li', "Mã sinh viên yêu cầu không để trống");
+        $this->assertQueryContentContains('li', "Tên sinh viên yêu cầu không để trống");
+        $this->assertQueryContentContains('li', "Ngày sinh yêu cầu không được để trống");
+        $this->assertQueryContentContains('li', "Giới tính yêu cầu không được để trống");
+        $this->assertQueryContentContains('li', "Số điện thoại yêu cầu không để trống");
+        $this->assertQueryContentContains('li', "Địa chỉ yêu cầu không để trống");
     }
-    
 
 }
