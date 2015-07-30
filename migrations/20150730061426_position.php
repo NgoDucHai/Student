1f67c882e06aa547bdcfad8f6d83bd40e737d737
tran-van-hoang <butdatac@gmail.com>
2015-07-30 15:01:48 +0700

    edit table position

--- migrations/20150730061426_position.php
+++ migrations/20150730061426_position.php
@@ -8,7 +8,7 @@ class Position extends AbstractMigration {
     public function change() {
         $table = $this->table('position', ['id' => false, 'primary_key' => ['positionId']]);
         $table->addColumn('positionId', 'string', ['limit' => 25])
-                ->addColumn('roleName', 'string', ['limit' => 50])
+                ->addColumn('positionName', 'string', ['limit' => 50])
                 ->create();
     }
 
